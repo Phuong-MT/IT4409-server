c25431eddeceeb7756b385596da575ab69a9abba
hquant1k29 <nonamentt2212004@gmail.com>
2025-10-26 02:32:46 +0700

    add register function

---
+++ src/controllers/authController.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import { User } from '../shared/models/user-model';
+import UserModel from '../models/user-model.mongo';
+import bcrypt from 'bcrypt';
+
+export const signUp = async (req: any, res: any) => {
+    try {
+         const body = req.body;
+         const { username, password, email, phoneNumber, dateOfBirth, address } = body;
+
+         if(!username || !password || !email || !phoneNumber || !dateOfBirth) {
+            return res
+            .status(400)
+            .json({ 
+                message: "Missing required fields" 
+            });
+         }
+
+         // Check if user already exists (pseudo code)
+        const userExists = await UserModel.findOne({ username });
+        if(userExists) {
+            return res
+            .status(409)
+            .json({ 
+                message: "User already exists" 
+            });
+        }
+
+        // hash password (pseudo code)
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // Create new user (pseudo code)
+        await UserModel.create({
+            userName: username,
+            password: hashedPassword,
+            email: email,
+            phoneNumber: phoneNumber,
+            dateOfBirth: dateOfBirth,
+            address: address || [] 
+        });
+        return res
+            .status(201)
+            .json({ 
+                message: "User registered successfully" 
+            });
+
+    } catch (error) {
+        console.error("Error in signUp:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+   
+}
--- src/routes/login.router.ts
+++ src/routes/login.router.ts
@@ -1,11 +1,16 @@
+import { sign } from "crypto";
 import express from "express";
+import { signUp } from "../controllers/authController";
 
 const LoginRouter = express.Router();
 
 LoginRouter.post("/auth/login", async (req, res) => {
+    
     const body = req.body;
     console.log("body data: ", body);
     res.status(200).json("success");
 });
+LoginRouter.post("/auth/register", signUp);
+
 
 export default LoginRouter;
