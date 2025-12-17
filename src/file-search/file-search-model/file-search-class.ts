32613f5dfc9d29eb32b3016988b908ee4eba00ec
minh phuong <minhphuongyenky@gmail.com>
2025-12-17 11:30:42 +0700

    feat: file search query

--- src/file-search/file-search-model/file-search-class.ts
+++ src/file-search/file-search-model/file-search-class.ts
@@ -1,6 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 import { GenAIClientManager } from "./gen-ai-client-manager";
 import FileSearchModel from "../../models/file-search.mongo";
+import fs from "fs";
 
 class FileSearchService {
     private client: GoogleGenAI;
@@ -49,15 +50,90 @@ class FileSearchService {
         });
     }
 
-    // async searchFile(query: string) {
-    //     return this.client.models.generateContent({
-    //         model: "models/gemini-1.5-flash",
-    //         contents: query,
-    //     });
-    // }
+    async uploadFile(filePath: string, fileName: string, typeFile?: string) {
+        if (this.store?.storeName.length > 0) {
+            const buffer = fs.readFileSync(filePath);
 
-    async searchByFileId(fileId: string, query: string) {
-        // implement logic search theo file
+            const blob = new Blob([buffer], {
+                type: "application/json",
+            });
+
+            console.log(blob);
+
+            let operation =
+                await this.client.fileSearchStores.uploadToFileSearchStore({
+                    file: blob,
+                    fileSearchStoreName: this.store.storeName,
+                    config: {
+                        displayName: fileName,
+                    },
+                });
+
+            while (!operation.done) {
+                await new Promise((r) => setTimeout(r, 1000));
+                operation = await this.client.operations.get({ operation });
+            }
+
+            if (operation.error) {
+                console.error("Upload failed:", operation.error);
+                return;
+            }
+
+            console.log("✅ Upload OK");
+            console.log(
+                "Response:",
+                JSON.stringify(operation.response, null, 2)
+            );
+            await FileSearchModel.findOneAndUpdate(
+                { storeName: this.store.storeName },
+                {
+                    $push: {
+                        fileId: {
+                            name: fileName,
+                            idFile: operation.response?.documentName,
+                        },
+                    },
+                },
+                { new: true }
+            );
+        } else {
+            console.log("Upload file to store error");
+        }
+    }
+
+    async queryRAG(contents: string) {
+        const response = await this.client.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents,
+            config: {
+                tools: [
+                    {
+                        fileSearch: {
+                            fileSearchStoreNames: [this.store.storeName],
+                        },
+                    },
+                ],
+            },
+        });
+        return response.text;
+    }
+
+    async queryRAGWithStream(contents: string, schemaResponse?: any) {
+        const stream = await this.client.models.generateContentStream({
+            model: "gemini-1.5-flash",
+            contents,
+            config: {
+                responseSchema: schemaResponse,
+                tools: [
+                    {
+                        fileSearch: {
+                            fileSearchStoreNames: [this.store.storeName],
+                        },
+                    },
+                ],
+            },
+        });
+        return stream;
     }
 }
 
--- src/routes/test.router.ts
+++ src/routes/test.router.ts
@@ -1,38 +1,39 @@
 import express from "express";
-// import {
-//     listDocumentName,
-//     testSearchFile,
-//     testUploadOneFile,
-// } from "../file-search";
+import fs from "fs";
+import { fileSearchService } from "../file-search/file-search-model/file-search-class";
+import path from "path";
 
 const testRouter = express.Router();
 
-// testRouter.post("/test/upload-file-search", async (req, res) => {
-//     try {
-//         await testUploadOneFile();
-//         return res.status(200).json("Oke");
-//     } catch (err) {
-//         console.log(err);
-//         return res.status(500).json("upload file search error");
-//     }
-// });
-// testRouter.post("/test/file-list", async (req, res) => {
-//     try {
-//         await listDocumentName();
-//         return res.status(200).json("Oke");
-//     } catch (err) {
-//         console.log(err);
-//         return res.status(500).json("list file name error");
-//     }
-// });
+testRouter.post("/test/upload-file", async (req, res) => {
+    try {
+        const filePath = path.resolve(
+            process.cwd(),
+            "src/file-search/data/mobile.json"
+        );
+        await fileSearchService.uploadFile(filePath, "mobile");
+        return res.status(200).json("oke");
+    } catch (err: any) {
+        console.log("upload file error: ", err);
+        return res.status(500).json("Error");
+    }
+});
 
-// testRouter.post("/test/query-search", async (req, res) => {
-//     try {
-//         await testSearchFile();
-//         return res.status(200).json("Oke");
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json("query-search error");
-//     }
-// });
+testRouter.post("/test/query", async (req, res) => {
+    try {
+        const contents = `
+            Tôi muốn tìm 1 chiếc dien thoai chup hinh tot nhat.
+        `;
+        const prompt = {
+            role: "user",
+            content: contents,
+        };
+        const result = await fileSearchService.queryRAG(JSON.stringify(prompt));
+        return res.status(200).json(result);
+    } catch (err: any) {
+        console.log("Query rag error: ", err);
+        return res.status(500).json("Error");
+    }
+});
+
 export default testRouter;
